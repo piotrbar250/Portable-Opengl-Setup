9b5ea1a3f348d343e457eab8b2209748cc3a3942
Piotr Baranowski <piotr.baranowski250@gmail.com>
2023-12-28 23:17:12 +0100

    Initial create

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+// #include <iostream>
+// #include <cmath>
+// #include "glad/glad.h"
+// #include "GLFW/glfw3.h"
+
+// int main()
+// {
+//     if (!glfwInit())
+//         return -1;
+
+//     GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//     if (!window)
+//     {
+//         glfwTerminate();
+//         return -1;
+//     }
+    
+//     glfwMakeContextCurrent(window);
+//     glfwSwapInterval(1);
+
+//     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+//     {
+//         std::cout << "Couldn't load opengl" << std::endl;
+//         glfwTerminate();
+//         return -1;
+//     }
+
+//     glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+
+//     while (!glfwWindowShouldClose(window))
+//     {
+//         glfwPollEvents();
+
+//         glClear(GL_COLOR_BUFFER_BIT);
+
+//         glfwSwapBuffers(window);
+//     }
+
+//     glfwTerminate();
+
+//     return 0;
+// }
---
+++ src/mainImgui.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cmath>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_internal.h"
+
+#include "UI.h"
+
+// int mainImgui()
+int main()
+{
+    if (!glfwInit())
+        return -1;
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char *glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char *glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char *glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Couldn't load opengl" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+
+    bool simulation = true;
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwWaitEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        simulation = ToggleSwitch("Toggle", simulation);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+
+    return 0;
+}
